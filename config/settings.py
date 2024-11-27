7b592d263038bf3ae024247363e1c9034bfaec40
Dev Ray <klantrilogy@gmail.com>
2024-11-27 23:03:06 +0300

    Fix static files configuration and security settings

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fy9#j&cy)*ew+dj80$)z%w@*h)voon&nv$o=x($(_k4!j(@e@m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['DevRay.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -155,14 +155,21 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
+# Security settings
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = False  # Set to True if you have SSL certificate
 
 # Tailwind Config
 TAILWIND_APP_NAME = 'theme'
